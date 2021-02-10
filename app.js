94d40cbe853a1ece236375ac0a7c0648056b1520
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-02-09 23:34:31 -0300

    added new route to get post values of form

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 var port = 3001;
 
+//config frontend denpendencies
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//config bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//routes
 app.get("/", (req, res) => {
 	res.render("index");
 });
@@ -14,7 +21,12 @@ app.get("/ask", (req, res) => {
 });
 
 app.post("/save-ask", (req, res) => {
-	res.send("formulário recebido");
+	// res.send("formulário recebido");
+
+	var title = req.body.title;
+	var describe = req.body.describe;
+
+	res.send("Dados foram: "+ title + describe)
 });
 
 app.listen(port, () => {console.log("Aplicação rodando na porta: "+ port)})
