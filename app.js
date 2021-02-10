8e836f2bb5304df61f4cf89a4d9e43bc0c76114e
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-02-09 23:02:18 -0300

    initialize project with frontend

--- app.js
+++ app.js
@@ -3,33 +3,46 @@ const app = express();
 var port = 3001;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-	// res.send("Bem vindo ao meu site.");
-	var name = "Jeremias";
-	var type = "Geral";
-
-	var showMsg = false;
-
-	var products = [
-		{
-			name: "Potato",
-		 	price: 5.00
-		},
-		{
-			name: "Fork",
-		 	price: 3.00
-		},
-
-	]
-
-
-	res.render("index",{
-		name: name,
-		type: type,
-		showMsg: showMsg,
-		products: products
-	});
+	res.render("index");
 });
+
+app.get("/ask", (req, res) => {
+	res.render("ask");
+});
+
+
+
+
+
+// app.get("/", (req, res) => {
+// 	// res.send("Bem vindo ao meu site.");
+// 	var name = "Jeremias";
+// 	var type = "Geral";
+
+// 	var showMsg = false;
+
+// 	var products = [
+// 		{
+// 			name: "Potato",
+// 		 	price: 5.00
+// 		},
+// 		{
+// 			name: "Fork",
+// 		 	price: 3.00
+// 		},
+
+// 	]
+
+
+// 	res.render("index",{
+// 		name: name,
+// 		type: type,
+// 		showMsg: showMsg,
+// 		products: products
+// 	});
+// });
 
 app.listen(port, () => {console.log("Aplicação rodando na porta: "+ port)})
