6f428ff6e73bb5ec5491e47b5ee2d8dcbeed9782
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-02-09 23:25:46 -0300

    added decoupling in redundant components

--- app.js
+++ app.js
@@ -13,36 +13,8 @@ app.get("/ask", (req, res) => {
 	res.render("ask");
 });
 
-
-
-
-
-// app.get("/", (req, res) => {
-// 	// res.send("Bem vindo ao meu site.");
-// 	var name = "Jeremias";
-// 	var type = "Geral";
-
-// 	var showMsg = false;
-
-// 	var products = [
-// 		{
-// 			name: "Potato",
-// 		 	price: 5.00
-// 		},
-// 		{
-// 			name: "Fork",
-// 		 	price: 3.00
-// 		},
-
-// 	]
-
-
-// 	res.render("index",{
-// 		name: name,
-// 		type: type,
-// 		showMsg: showMsg,
-// 		products: products
-// 	});
-// });
+app.post("/save-ask", (req, res) => {
+	res.send("formulário recebido");
+});
 
 app.listen(port, () => {console.log("Aplicação rodando na porta: "+ port)})
